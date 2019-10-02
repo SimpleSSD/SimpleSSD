02340fcc71d61bb16eaa91e7493b9d2e7a80cd28
Donghyun Gouk <kukdh1@gmail.com>
2019-10-02 15:30:00 +0900

    util: Add FIFO from v2.0.12

---
+++ util/fifo.cc
@@ -0,0 +1,425 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#include "util/fifo.hh"
+
+#include "util/algorithm.hh"
+
+namespace SimpleSSD {
+
+FIFOEntry::FIFOEntry()
+    : last(true),
+      id(0),
+      addr(0),
+      size(0),
+      buffer(nullptr),
+      arrivedAt(0),
+      insertBeginAt(0),
+      insertEndAt(0),
+      eid(InvalidEventID),
+      context(nullptr) {}
+
+FIFOEntry::FIFOEntry(uint64_t a, uint64_t s, uint8_t *b, uint64_t t, Event e,
+                     void *c)
+    : last(true),
+      id(0),
+      addr(a),
+      size(s),
+      buffer(b),
+      arrivedAt(t),
+      insertBeginAt(0),
+      insertEndAt(0),
+      eid(e),
+      context(c) {}
+
+ReadEntry::ReadEntry() : id(0), insertEndAt(0), dmaEndAt(0), latency(0) {}
+
+ReadEntry::ReadEntry(uint64_t a, uint64_t i, uint64_t d, uint64_t l)
+    : id(a), insertEndAt(i), dmaEndAt(d), latency(l) {}
+
+FIFO::Queue::Queue(uint64_t c)
+    : capacity(c), usage(0), insertPending(false), transferPending(false) {}
+
+FIFO::FIFO(ObjectData &o, DMAInterface *up, FIFOParam &p)
+    : Object(o),
+      upstream(up),
+      param(p),
+      readQueue(p.rqSize),
+      writeQueue(p.wqSize),
+      counter(0) {
+  // Sanity check
+  panic_if(
+      param.transferUnit > param.rqSize || param.transferUnit > param.wqSize,
+      "Invalid transferUnit size");
+
+  // Set Transfer Unit Latency
+  unitLatency = param.latency(param.transferUnit);
+
+  // Allocate events and functions for each queue
+  writeQueue.insertDone =
+      createEvent([this](uint64_t, void *) { insertWriteDone(); },
+                  "FIFO::writeQueue::insertDone");
+  writeQueue.beginTransfer =
+      createEvent([this](uint64_t, void *) { transferWrite(); },
+                  "FIFO::writeQueue::beginTransfer");
+  writeQueue.submitCompletion =
+      createEvent([this](uint64_t, void *) { transferWriteDoneNext(); },
+                  "FIFO::writeQueue::submitCompletion");
+  writeQueue.transferDone =
+      createEvent([this](uint64_t, void *) { transferWriteDone(); },
+                  "FIFO::writeQueue::transferDone");
+  readQueue.insertDone =
+      createEvent([this](uint64_t, void *) { insertReadDone(); },
+                  "FIFO::readQueue::insertDone");
+  readQueue.beginTransfer =
+      createEvent([this](uint64_t, void *) { insertRead(); },
+                  "FIFO::readQueue::beginTransfer");
+  readQueue.submitCompletion =
+      createEvent([this](uint64_t, void *) { insertReadDoneNext(); },
+                  "FIFO::readQueue::submitCompletion");
+  readQueue.transferDone =
+      createEvent([this](uint64_t, void *) { transferReadDone(); },
+                  "FIFO::readQueue::transferDone");
+}
+
+FIFO::~FIFO() {}
+
+std::list<ReadEntry>::iterator FIFO::find(uint64_t id) {
+  auto iter = readCompletion.begin();
+
+  for (; iter != readCompletion.end(); iter++) {
+    if (iter->id == id) {
+      break;
+    }
+  }
+
+  return iter;
+}
+
+uint64_t FIFO::calcSize(uint64_t size, bool &b) {
+  if (size < param.transferUnit) {
+    b = true;
+
+    return size;
+  }
+
+  return DIVCEIL(size, param.transferUnit) * param.transferUnit;
+}
+
+void FIFO::insertWrite() {
+  auto iter = writeQueue.waitQueue.begin();
+  bool smallerThanUnit = false;
+  uint64_t now = getTick();
+  uint64_t size = calcSize(iter->size, smallerThanUnit);
+
+  if ((writeQueue.insertPending ||
+       writeQueue.usage + size > writeQueue.capacity) &&
+      size <= param.wqSize) {
+    return;
+  }
+
+  // We need to split large request
+  if (size > param.wqSize) {
+    FIFOEntry copy = *iter;
+
+    // copy is next one
+    copy.last = true;
+    copy.addr = iter->addr + param.transferUnit;
+    copy.size = iter->size - param.transferUnit;
+    copy.buffer = iter->buffer ? iter->buffer + param.transferUnit : nullptr;
+
+    // iter is current one
+    iter->last = false;
+    iter->size = param.transferUnit;
+
+    // Insert copy after iter
+    writeQueue.waitQueue.insert(++iter, copy);
+
+    // iter should first one
+    iter = writeQueue.waitQueue.begin();
+    size = calcSize(iter->size, smallerThanUnit);
+  }
+
+  writeQueue.insertPending = true;
+
+  // Current item will be written to FIFO
+  writeQueue.usage += size;
+  iter->insertBeginAt = now;
+  iter->insertEndAt = now + param.latency(size);
+
+  // When insertion done, call handler
+  schedule(writeQueue.insertDone, iter->insertEndAt);
+
+  // Current item will be transferred after one transferUnit written to FIFO
+  // If we alwritey scheduled, current item will be transffered after last one
+  if (!isScheduled(writeQueue.beginTransfer)) {
+    schedule(writeQueue.beginTransfer,
+             now + (smallerThanUnit ? param.latency(size) : unitLatency));
+  }
+
+  // Push current item to transferQueue
+  writeQueue.transferQueue.push(*iter);
+}
+
+void FIFO::insertWriteDone() {
+  // Pop last item
+  writeQueue.waitQueue.pop_front();
+  writeQueue.insertPending = false;
+
+  // Insert next waiting item
+  if (writeQueue.waitQueue.size() > 0) {
+    insertWrite();
+  }
+}
+
+void FIFO::transferWrite() {
+  auto &iter = writeQueue.transferQueue.front();
+
+  if (writeQueue.transferPending) {
+    return;
+  }
+
+  writeQueue.transferPending = true;
+
+  // Begin transfer
+  upstream->write(iter.addr, iter.size, iter.buffer, writeQueue.transferDone);
+}
+
+void FIFO::transferWriteDone() {
+  auto &iter = writeQueue.transferQueue.front();
+  uint64_t now = getTick();
+  uint64_t latency =
+      (iter.size < param.transferUnit) ? param.latency(iter.size) : unitLatency;
+
+  if (now >= iter.insertEndAt + latency) {  // Upstream <= downstream
+    // Finish immediately
+    transferWriteDoneNext();
+  }
+  else {  // upstream is faster than downstream
+    // Should finish at insertedEndAt + upstreamLatency(transferUnit)
+    // TODO: Applying unitLatency is not correct (more slower result)
+    schedule(writeQueue.submitCompletion, iter.insertEndAt + latency);
+  }
+}
+
+void FIFO::transferWriteDoneNext() {
+  auto &iter = writeQueue.transferQueue.front();
+  bool unused;
+  uint64_t now = getTick();
+
+  // Call handler
+  if (iter.last) {
+    schedule(iter.eid, now, iter.context);
+  }
+
+  // Transfer done
+  writeQueue.usage -= calcSize(iter.size, unused);
+  writeQueue.transferPending = false;
+  writeQueue.transferQueue.pop();
+
+  // Do next transfer if we have
+  if (writeQueue.transferQueue.size() > 0) {
+    if (!isScheduled(writeQueue.beginTransfer)) {
+      transferWrite();
+    }
+  }
+
+  // Do next insertion if we have
+  if (writeQueue.waitQueue.size() > 0) {
+    insertWrite();
+  }
+}
+
+void FIFO::transferRead() {
+  auto iter = readQueue.waitQueue.begin();
+  bool smallerThanUnit = false;
+  uint64_t size = calcSize(iter->size, smallerThanUnit);
+
+  if ((readQueue.transferPending ||
+       readQueue.usage + size > readQueue.capacity) &&
+      size <= param.rqSize) {
+    return;
+  }
+
+  // We need to split large request
+  if (size > param.rqSize) {
+    FIFOEntry copy = *iter;
+
+    // copy is next one
+    copy.last = true;
+    copy.addr = iter->addr + param.transferUnit;
+    copy.size = iter->size - param.transferUnit;
+    copy.buffer = iter->buffer ? iter->buffer + param.transferUnit : nullptr;
+
+    // iter is current one
+    iter->last = false;
+    iter->size = param.transferUnit;
+
+    // Insert copy after iter
+    readQueue.waitQueue.insert(++iter, copy);
+
+    // iter should first one
+    iter = readQueue.waitQueue.begin();
+    size = calcSize(iter->size, smallerThanUnit);
+  }
+
+  readQueue.transferPending = true;
+
+  // Set ID
+  iter->id = counter++;
+
+  // Current item will be requested to be read
+  readQueue.usage += size;
+
+  // When all data is read to FIFO, call handler
+  upstream->read(iter->addr, iter->size, iter->buffer, readQueue.transferDone);
+
+  if (!isScheduled(readQueue.beginTransfer)) {
+    schedule(readQueue.beginTransfer,
+             getTick() + (smallerThanUnit ? param.latency(size) : unitLatency));
+  }
+
+  // Push current item to transferQueue
+  readQueue.transferQueue.push(*iter);
+}
+
+void FIFO::transferReadDone() {
+  auto &iter = readQueue.waitQueue.front();
+  uint64_t latency =
+      (iter.size < param.transferUnit) ? param.latency(iter.size) : unitLatency;
+
+  // Save DMA end time
+  auto comp = find(iter.id);
+
+  if (comp == readCompletion.end()) {
+    readCompletion.push_back(ReadEntry(iter.id, 0, getTick(), latency));
+  }
+  else {
+    comp->dmaEndAt = getTick();
+
+    insertReadDoneMerge(comp);
+  }
+
+  // Pop last item
+  readQueue.waitQueue.pop_front();
+  readQueue.transferPending = false;
+
+  // Transfer next waiting item
+  if (readQueue.waitQueue.size() > 0) {
+    transferRead();
+  }
+}
+
+void FIFO::insertRead() {
+  auto &iter = readQueue.transferQueue.front();
+  bool unused;
+  uint64_t now = getTick();
+
+  if (readQueue.insertPending) {
+    return;
+  }
+
+  readQueue.insertPending = true;
+
+  // Begin insertion
+  iter.insertBeginAt = now;
+  iter.insertEndAt = now + param.latency(calcSize(iter.size, unused));
+
+  // When insertion done, call handler
+  if (!isScheduled(readQueue.insertDone)) {
+    schedule(readQueue.insertDone, iter.insertEndAt);
+  }
+}
+
+void FIFO::insertReadDone() {
+  auto &iter = readQueue.transferQueue.front();
+  uint64_t latency =
+      (iter.size < param.transferUnit) ? param.latency(iter.size) : unitLatency;
+
+  auto comp = find(iter.id);
+
+  if (comp == readCompletion.end()) {
+    readCompletion.push_back(ReadEntry(iter.id, getTick(), 0, latency));
+  }
+  else {
+    insertReadDoneMerge(comp);
+  }
+}
+
+void FIFO::insertReadDoneMerge(std::list<ReadEntry>::iterator comp) {
+  uint64_t now = getTick();
+
+  if (now >= comp->dmaEndAt + comp->latency) {  // Upstream <= downstream
+    // Finish immediately
+    insertReadDoneNext();
+  }
+  else {  // upstream is faster than downstream
+    // Should finish at insertedEndAt + upstreamLatency(transferUnit)
+    // TODO: Applying unitLatency is not correct (more slower result)
+    schedule(readQueue.submitCompletion, comp->dmaEndAt + comp->latency);
+  }
+
+  readCompletion.erase(comp);
+}
+
+void FIFO::insertReadDoneNext() {
+  auto &iter = readQueue.transferQueue.front();
+  bool unused;
+  uint64_t now = getTick();
+
+  // Call handler
+  if (iter.last) {
+    schedule(iter.eid, now, iter.context);
+  }
+
+  // Insert done
+  readQueue.usage -= calcSize(iter.size, unused);
+  readQueue.insertPending = false;
+  readQueue.transferQueue.pop();
+
+  // Do next insert if we have
+  if (readQueue.transferQueue.size() > 0) {
+    if (!isScheduled(readQueue.beginTransfer)) {
+      insertRead();
+    }
+  }
+
+  // Do next transfer if we have
+  if (readQueue.waitQueue.size() > 0) {
+    transferRead();
+  }
+}
+
+void FIFO::read(uint64_t addr, uint64_t size, uint8_t *buffer, Event eid,
+                void *context) {
+  if (size == 0) {
+    warn("FIFO: zero-size DMA read request. Ignore.");
+
+    return;
+  }
+
+  readQueue.waitQueue.push_back(
+      FIFOEntry(addr, size, buffer, getTick(), eid, context));
+
+  transferRead();
+}
+
+void FIFO::write(uint64_t addr, uint64_t size, uint8_t *buffer, Event eid,
+                 void *context) {
+  if (size == 0) {
+    warn("FIFO: zero-size DMA write request. Ignore.");
+
+    return;
+  }
+
+  writeQueue.waitQueue.push_back(
+      FIFOEntry(addr, size, buffer, getTick(), eid, context));
+
+  insertWrite();
+}
+
+}  // namespace SimpleSSD
---
+++ util/fifo.hh
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#pragma once
+
+#ifndef __UTIL_FIFO__
+#define __UTIL_FIFO__
+
+#include <list>
+#include <queue>
+
+#include "sim/interface.hh"
+#include "sim/object.hh"
+#include "util/interface.hh"
+
+namespace SimpleSSD {
+
+struct FIFOParam {
+  uint64_t rqSize;        //!< Read Queue size
+  uint64_t wqSize;        //!< Write Queue size
+  uint64_t transferUnit;  //!< Transfer unit used to interleave DMA
+  DelayFunction latency;  //!< Downstream latency function
+};
+
+struct FIFOEntry {
+  bool last;  //!< This is last entry of splitted request
+  uint64_t id;
+
+  uint64_t addr;
+  uint64_t size;
+  uint8_t *buffer;
+
+  uint64_t arrivedAt;      //!< Request arrived to FIFO
+  uint64_t insertBeginAt;  //!< Request is now begin to written on FIFO
+  uint64_t insertEndAt;    //!< Request is fully written on FIFO
+
+  Event eid;
+  void *context;
+
+  FIFOEntry();
+  FIFOEntry(uint64_t, uint64_t, uint8_t *, uint64_t, Event, void *);
+};
+
+struct ReadEntry {
+  uint64_t id;
+  uint64_t insertEndAt;
+  uint64_t dmaEndAt;
+  uint64_t latency;
+
+  ReadEntry();
+  ReadEntry(uint64_t, uint64_t, uint64_t, uint64_t);
+};
+
+class FIFO : public DMAInterface, public Object {
+ protected:
+  class Queue {
+   public:
+    uint64_t capacity;
+    uint64_t usage;
+
+    std::list<FIFOEntry> waitQueue;
+    std::queue<FIFOEntry> transferQueue;
+
+    Event insertDone;
+    Event beginTransfer;
+    Event submitCompletion;
+    Event transferDone;
+
+    bool insertPending;
+    bool transferPending;
+
+    Queue(uint64_t);
+  };
+
+  DMAInterface *upstream;
+  FIFOParam param;
+  uint64_t unitLatency;
+
+  Queue readQueue;
+  Queue writeQueue;
+
+  uint64_t counter;
+  std::list<ReadEntry> readCompletion;
+
+  std::list<ReadEntry>::iterator find(uint64_t);
+
+  uint64_t calcSize(uint64_t, bool &);
+
+  // Write
+  void insertWrite();
+  void insertWriteDone();
+  void transferWrite();
+  void transferWriteDone();
+  void transferWriteDoneNext();
+
+  // Read
+  void transferRead();
+  void transferReadDone();
+  void insertRead();
+  void insertReadDone();
+  void insertReadDoneMerge(std::list<ReadEntry>::iterator);
+  void insertReadDoneNext();
+
+ public:
+  FIFO() = delete;
+  FIFO(ObjectData &, DMAInterface *, FIFOParam &);
+  FIFO(const FIFO &) = delete;
+  FIFO(FIFO &&) noexcept = default;
+  ~FIFO();
+
+  FIFO &operator=(const FIFO &) = delete;
+  FIFO &operator=(FIFO &&) noexcept = default;
+
+  void read(uint64_t, uint64_t, uint8_t *, Event, void * = nullptr) override;
+  void write(uint64_t, uint64_t, uint8_t *, Event, void * = nullptr) override;
+};
+
+}  // namespace SimpleSSD
+
+#endif
