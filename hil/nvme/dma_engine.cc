820bdcbe6b82f4fdb3ae10f969f3826eb4d4932b
Donghyun Gouk <kukdh1@gmail.com>
2019-10-02 23:02:40 +0900

    hil,nvme: Add PRPEngine

---
+++ hil/nvme/dma_engine.cc
@@ -0,0 +1,171 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#include "hil/nvme/dma_engine.hh"
+
+namespace SimpleSSD::HIL::NVMe {
+
+PRP::PRP() : address(0), size(0) {}
+
+PRP::PRP(uint64_t a, uint64_t s) : address(a), size(s) {}
+
+PRPEngine::PRPEngine(ObjectData &&o, Interface *i, uint64_t p)
+    : DMAEngine(std::move(o), i), inited(false), totalSize(0), pageSize(p) {
+  panic_if(popcount(p) != 1, "Invalid memory page size provided.");
+
+  readPRPList = createEvent(
+      [this](uint64_t t, void *c) {
+        getPRPListFromPRP_readDone(t, (PRPInitContext *)c);
+      },
+      "HIL::NVMe::PRPEngine::readPRPList");
+}
+
+PRPEngine::~PRPEngine() {}
+
+void PRPEngine::getPRPListFromPRP_readDone(uint64_t now, PRPInitContext *data) {
+  uint64_t listPRP;
+  uint64_t listPRPSize;
+
+  for (size_t i = 0; i < data->bufferSize; i += 8) {
+    listPRP = *((uint64_t *)(data->buffer + i));
+    listPRPSize = getSizeFromPRP(listPRP);
+
+    if (listPRP == 0) {
+      panic("prp_list: Invalid PRP in PRP List");
+    }
+
+    data->handledSize += listPRPSize;
+    prpList.push_back(PRP(listPRP, listPRPSize));
+
+    if (data->handledSize >= totalSize) {
+      break;
+    }
+  }
+
+  free(data->buffer);
+
+  if (data->handledSize < totalSize) {
+    // PRP list ends but size is not full
+    // Last item of PRP list is pointer of another PRP list
+    listPRP = prpList.back().address;
+    listPRPSize = getSizeFromPRP(listPRP);
+
+    prpList.pop_back();
+
+    data->bufferSize = listPRPSize;
+    data->buffer = (uint8_t *)malloc(data->bufferSize);
+
+    pInterface->read(listPRP, data->bufferSize, data->buffer, readPRPList,
+                     data);
+  }
+  else {
+    // Done
+    inited = true;
+
+    schedule(data->eid, now, data->context);
+
+    delete data;
+  }
+}
+
+uint64_t PRPEngine::getSizeFromPRP(uint64_t prp) {
+  return pageSize - (prp & (pageSize - 1));
+}
+
+void PRPEngine::getPRPListFromPRP(uint64_t prp, Event eid, void *context) {
+  auto data = new PRPInitContext();
+
+  data->handledSize = 0;
+  data->eid = eid;
+  data->context = context;
+  data->bufferSize = getSizeFromPRP(prp);
+  data->buffer = (uint8_t *)malloc(data->bufferSize);
+
+  pInterface->read(prp, data->bufferSize, data->buffer, readPRPList, data);
+}
+
+void PRPEngine::initData(uint64_t prp1, uint64_t prp2, uint64_t sizeLimit,
+                         Event eid, void *context) {
+  bool immediate = true;
+  uint8_t mode = 0xFF;
+  uint64_t prp1Size = getSizeFromPRP(prp1);
+  uint64_t prp2Size = getSizeFromPRP(prp2);
+
+  totalSize = sizeLimit;
+
+  // Determine PRP1 and PRP2
+  if (totalSize <= pageSize) {
+    if (totalSize <= prp1Size) {
+      mode = 0;
+    }
+    else {
+      mode = 1;
+    }
+  }
+  else if (totalSize <= pageSize * 2) {
+    if (prp1Size == pageSize) {
+      mode = 1;
+    }
+    else {
+      mode = 2;
+    }
+  }
+  else {
+    mode = 2;
+  }
+
+  switch (mode) {
+    case 0:
+      // PRP1 is PRP pointer, PRP2 is not used
+      prpList.push_back(PRP(prp1, totalSize));
+
+      break;
+    case 1:
+      // PRP1 and PRP2 are PRP pointer
+      prpList.push_back(PRP(prp1, prp1Size));
+      prpList.push_back(PRP(prp2, prp2Size));
+
+      panic_if(prp1Size + prp2Size < totalSize, "Invalid DPTR size");
+
+      break;
+    case 2:
+      immediate = false;
+
+      // PRP1 is PRP pointer, PRP2 is PRP list
+      prpList.push_back(PRP(prp1, prp1Size));
+      getPRPListFromPRP(prp2, eid, context);
+
+      break;
+    default:
+      panic("Invalid PRP1/2 while parsing.");
+
+      break;
+  }
+
+  if (immediate) {
+    inited = true;
+
+    schedule(eid, getTick(), context);
+  }
+}
+
+void PRPEngine::initQueue(uint64_t base, uint64_t size, bool cont, Event eid,
+                          void *context) {
+  totalSize = size;
+
+  if (cont) {
+    prpList.push_back(PRP(base, size));
+
+    inited = true;
+    schedule(eid, getTick(), context);
+  }
+  else {
+    getPRPListFromPRP(base, eid, context);
+  }
+}
+
+}  // namespace SimpleSSD::HIL::NVMe
---
+++ hil/nvme/dma_engine.hh
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#pragma once
+
+#ifndef __HIL_NVME_DMA_ENGINE_HH__
+#define __HIL_NVME_DMA_ENGINE_HH__
+
+#include <vector>
+
+#include "hil/common/dma_engine.hh"
+
+namespace SimpleSSD::HIL::NVMe {
+
+class PRP {
+ public:
+  uint64_t address;
+  uint64_t size;
+
+  PRP();
+  PRP(uint64_t, uint64_t);
+};
+
+class PRPEngine : public DMAEngine {
+ private:
+  class PRPInitContext {
+   public:
+    uint64_t handledSize;
+    uint8_t *buffer;
+    uint64_t bufferSize;
+    Event eid;
+    void *context;
+  };
+
+  bool inited;
+
+  std::vector<PRP> prpList;
+  uint64_t totalSize;
+  uint64_t pageSize;
+
+  Event readPRPList;
+
+  uint64_t getSizeFromPRP(uint64_t);
+  void getPRPListFromPRP(uint64_t, Event, void *);
+  void getPRPListFromPRP_readDone(uint64_t, PRPInitContext *s);
+
+ public:
+  PRPEngine(ObjectData &&, Interface *, uint64_t);
+  ~PRPEngine();
+
+  void initData(uint64_t, uint64_t, uint64_t, Event, void * = nullptr);
+  void initQueue(uint64_t, uint64_t, bool, Event, void * = nullptr);
+
+  void read(uint64_t, uint64_t, uint8_t *, Event, void * = nullptr) override;
+  void write(uint64_t, uint64_t, uint8_t *, Event, void * = nullptr) override;
+};
+
+}  // namespace SimpleSSD::HIL::NVMe
+
+#endif
