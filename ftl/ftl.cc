0091901398af6477ad4fa8603d10f2364c267fc8
Donghyun Gouk <kukdh1@gmail.com>
2019-10-18 01:04:43 +0900

    ftl: Update ftl

--- ftl/ftl.cc
+++ ftl/ftl.cc
@@ -7,8 +7,6 @@
 
 #include "ftl/ftl.hh"
 
-#include "ftl/abstract_ftl.hh"
-
 namespace SimpleSSD {
 
 namespace FTL {
@@ -16,122 +14,70 @@ namespace FTL {
 FTL::FTL(ObjectData &o) : Object(o) {
   pFIL = new FIL::FIL(object);
 
-  auto filparam = object.config->getNANDStructure();
-  auto channel =
-      readConfigUint(Section::FlashInterface, FIL::Config::Key::Channel);
-  auto way = readConfigUint(Section::FlashInterface, FIL::Config::Key::Way);
-
-  param.totalPhysicalPages = channel * way * filparam->die * filparam->plane *
-                             filparam->block * filparam->plane;
-  param.totalLogicalPages =
-      param.totalPhysicalPages /
-      (1.f - readConfigFloat(Section::FlashTranslation,
-                             Config::Key::OverProvisioningRatio));
-  param.pageSize = filparam->pageSize;
-
-  // Yout MUST NOT REORDER Parameter structure
-  for (uint8_t i = 0; i < 4; i++) {
-    switch (filparam->pageAllocation[i]) {
-      case FIL::PageAllocation::Channel:
-        param.parallelismLevel[i] = channel;
-        break;
-      case FIL::PageAllocation::Way:
-        param.parallelismLevel[i] = way;
-        break;
-      case FIL::PageAllocation::Die:
-        param.parallelismLevel[i] = filparam->die;
-        break;
-      case FIL::PageAllocation::Plane:
-        param.parallelismLevel[i] = filparam->plane;
-        break;
-    }
-  }
-
-  if (readConfigBoolean(Section::FlashTranslation, Config::Key::UseSuperpage)) {
-    param.superpageLevel = readConfigUint(Section::FlashTranslation,
-                                          Config::Key::SuperpageAllocation);
-  }
-  else {
-    param.superpageLevel = FIL::PageAllocation::None;
-  }
-
-  // Validate superpage level
-  uint8_t mask = FIL::PageAllocation::None;
-
-  for (uint8_t i = 0; i < 4; i++) {
-    if (param.superpageLevel | filparam->pageAllocation[i]) {
-      mask |= filparam->pageAllocation[i];
-    }
-    else {
-      break;
-    }
-  }
-
-  panic_if(param.superpageLevel != mask,
-           "Invalid superpage configuration detected.");
-
-  param.superpageLevel = popcount16(mask);
-
   switch ((Config::MappingType)readConfigUint(Section::FlashTranslation,
                                               Config::Key::MappingMode)) {
     case Config::MappingType::PageLevelFTL:
+      // pMapper = new Mapping::PageLevel(object);
+
       break;
     case Config::MappingType::BlockLevelFTL:
+      // pMapper = new Mapping::BlockLevel(object);
+
       break;
     case Config::MappingType::VLFTL:
+      // pMapper = new Mapping::VLFTL(object);
+
       break;
   }
 
-  // Print mapping Information
-  debugprint(Log::DebugID::FTL, "Total physical pages %u",
-             param.totalPhysicalPages);
-  debugprint(Log::DebugID::FTL, "Total logical pages %u",
-             param.totalLogicalPages);
-  debugprint(Log::DebugID::FTL, "Logical page size %u", param.pageSize);
+  // Currently, we only have default block allocator
+  // pAllocator = new BlockAllocator::Default(object, pMapper);
 
   // Initialize pFTL
-  pFTL->initialize();
+  pMapper->initialize(pAllocator);
 }
 
 FTL::~FTL() {
   delete pFIL;
-  delete pFTL;
+  delete pMapper;
+  delete pAllocator;
 }
 
 Parameter *FTL::getInfo() {
-  return &param;
+  return pMapper->getInfo();
 }
 
 LPN FTL::getPageUsage(LPN lpnBegin, LPN lpnEnd) {
-  return pFTL->getStatus(lpnBegin, lpnEnd)->mappedLogicalPages;
+  return pMapper->getPageUsage(lpnBegin, lpnEnd);
 }
 
 void FTL::getStatList(std::vector<Stat> &list, std::string prefix) noexcept {
-  pFTL->getStatList(list, prefix + "ftl.");
+  pMapper->getStatList(list, prefix + "ftl.mapper.");
+  pAllocator->getStatList(list, prefix + "ftl.allocator.");
   pFIL->getStatList(list, prefix);
 }
 
 void FTL::getStatValues(std::vector<double> &values) noexcept {
-  pFTL->getStatValues(values);
+  pMapper->getStatValues(values);
+  pAllocator->getStatValues(values);
   pFIL->getStatValues(values);
 }
 
 void FTL::resetStatValues() noexcept {
-  pFTL->resetStatValues();
+  pMapper->resetStatValues();
+  pAllocator->resetStatValues();
   pFIL->resetStatValues();
 }
 
 void FTL::createCheckpoint(std::ostream &out) const noexcept {
-  BACKUP_BLOB(out, &param, sizeof(Parameter));
-
-  pFTL->createCheckpoint(out);
+  pMapper->createCheckpoint(out);
+  pAllocator->createCheckpoint(out);
   pFIL->createCheckpoint(out);
 }
 
 void FTL::restoreCheckpoint(std::istream &in) noexcept {
-  RESTORE_BLOB(in, &param, sizeof(Parameter));
-
-  pFTL->restoreCheckpoint(in);
+  pMapper->restoreCheckpoint(in);
+  pAllocator->restoreCheckpoint(in);
   pFIL->restoreCheckpoint(in);
 }
 
--- ftl/mapping/abstract_mapping.hh
+++ ftl/mapping/abstract_mapping.hh
@@ -29,7 +29,71 @@ class AbstractMapping : public Object {
   BlockAllocator::AbstractAllocator *allocator;
 
  public:
-  AbstractMapping(ObjectData &o) : Object(o), allocator(nullptr) {}
+  AbstractMapping(ObjectData &o) : Object(o), allocator(nullptr) {
+    auto filparam = object.config->getNANDStructure();
+    auto channel =
+        readConfigUint(Section::FlashInterface, FIL::Config::Key::Channel);
+    auto way = readConfigUint(Section::FlashInterface, FIL::Config::Key::Way);
+
+    param.totalPhysicalPages = channel * way * filparam->die * filparam->plane *
+                               filparam->block * filparam->plane;
+    param.totalLogicalPages =
+        param.totalPhysicalPages /
+        (1.f - readConfigFloat(Section::FlashTranslation,
+                               Config::Key::OverProvisioningRatio));
+    param.pageSize = filparam->pageSize;
+
+    for (uint8_t i = 0; i < 4; i++) {
+      switch (filparam->pageAllocation[i]) {
+        case FIL::PageAllocation::Channel:
+          param.parallelismLevel[i] = channel;
+          break;
+        case FIL::PageAllocation::Way:
+          param.parallelismLevel[i] = way;
+          break;
+        case FIL::PageAllocation::Die:
+          param.parallelismLevel[i] = filparam->die;
+          break;
+        case FIL::PageAllocation::Plane:
+          param.parallelismLevel[i] = filparam->plane;
+          break;
+      }
+    }
+
+    if (readConfigBoolean(Section::FlashTranslation,
+                          Config::Key::UseSuperpage)) {
+      param.superpageLevel = readConfigUint(Section::FlashTranslation,
+                                            Config::Key::SuperpageAllocation);
+    }
+    else {
+      param.superpageLevel = FIL::PageAllocation::None;
+    }
+
+    // Validate superpage level
+    uint8_t mask = FIL::PageAllocation::None;
+
+    for (uint8_t i = 0; i < 4; i++) {
+      if (param.superpageLevel | filparam->pageAllocation[i]) {
+        mask |= filparam->pageAllocation[i];
+      }
+      else {
+        break;
+      }
+    }
+
+    panic_if(param.superpageLevel != mask,
+             "Invalid superpage configuration detected.");
+
+    param.superpageLevel = popcount16(mask);
+
+    // Print mapping Information
+    debugprint(Log::DebugID::FTL, "Total physical pages %u",
+               param.totalPhysicalPages);
+    debugprint(Log::DebugID::FTL, "Total logical pages %u",
+               param.totalLogicalPages);
+    debugprint(Log::DebugID::FTL, "Logical page size %u", param.pageSize);
+  }
+
   AbstractMapping(const AbstractMapping &) = delete;
   AbstractMapping(AbstractMapping &&) noexcept = default;
   virtual ~AbstractMapping() {}
