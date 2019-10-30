0f61ba2feeb48aac121baad96c205ffd8ef1b1d6
Donghyun Gouk <kukdh1@gmail.com>
2019-10-30 22:22:05 +0900

    ftl,allocator: Add allocator which maintains two set of inUseBlockMap

---
+++ ftl/allocator/two_block_allocator.hh
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#pragma once
+
+#ifndef __SIMPLESSD_FTL_ALLOCATOR_TWO_BLOCK_ALLOCATOR_HH__
+#define __SIMPLESSD_FTL_ALLOCATOR_TWO_BLOCK_ALLOCATOR_HH__
+
+#include <list>
+#include <random>
+
+#include "ftl/allocator/basic_allocator.hh"
+
+namespace SimpleSSD::FTL::BlockAllocator {
+
+class TwoBlockAllocator : public BasicAllocator {
+ protected:
+  PPN lastAllocatedSecond;   // Used for pMapper->initialize
+  PPN *inUseBlockMapSecond;  // Allocated free blocks
+
+ public:
+  TwoBlockAllocator(ObjectData &, Mapping::AbstractMapping *);
+  ~TwoBlockAllocator();
+
+  void initialize(Parameter *) override;
+
+  CPU::Function allocateBlockSecond(PPN &);
+  PPN getBlockAtSecond(PPN);
+
+  bool stallRequest() override;
+};
+
+}  // namespace SimpleSSD::FTL::BlockAllocator
+
+#endif
