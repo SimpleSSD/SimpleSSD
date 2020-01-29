3d975913b212d30233180637a12f7667c69ef51b
Donghyun Gouk <kukdh1@gmail.com>
2020-01-30 02:05:45 +0900

    icl: Add comments

--- icl/cache/abstract_cache.hh
+++ icl/cache/abstract_cache.hh
@@ -22,16 +22,76 @@ class AbstractCache : public Object {
   static const uint64_t minIO = 512;
 
   AbstractManager *manager;
+  FTL::Parameter *parameter;
 
  public:
-  AbstractCache(ObjectData &o, AbstractManager *m) : Object(o), manager(m) {}
+  AbstractCache(ObjectData &o, AbstractManager *m, FTL::Parameter *p)
+      : Object(o), manager(m), parameter(p) {}
   virtual ~AbstractCache() {}
 
-  virtual CPU::Function lookup(SubRequest *, bool) = 0;
-  virtual CPU::Function allocate(SubRequest *) = 0;
-  virtual CPU::Function flush(SubRequest *) = 0;
-  virtual CPU::Function erase(SubRequest *) = 0;
-  virtual void dmaDone(LPN) = 0;
+  /**
+   * \brief Lookup cache
+   *
+   * Set sreq->setHit() when current subrequest is found on cache.
+   * When read, setHit() should be called when cache hit.
+   * When write, setHit() should be called when cache hit and cold miss.
+   *
+   * \param[in] sreq    Current subrequest
+   * \param[in] isRead  True when read
+   */
+  virtual CPU::Function lookup(SubRequest *sreq, bool isRead) = 0;
+
+  /**
+   * \brief Allocate cacheline in cache
+   *
+   * When miss, cache should make place to store new data. Call
+   * AbstractManager::drain for data write-back.
+   *
+   * \param[in] sreq  Current subrequest
+   */
+  virtual CPU::Function allocate(SubRequest *sreq) = 0;
+
+  /**
+   * \brief Flush cacheline
+   *
+   * Flush cacheline in [offset, offset + length) range.
+   *
+   * \param[in] sreq  Current subrequest
+   */
+  virtual CPU::Function flush(SubRequest *sreq) = 0;
+
+  /**
+   * \brief Erase cacheline
+   *
+   * Erase (invalidate) cacheline in [offset, offset + length) range.
+   *
+   * \param[in] sreq  Current subrequest
+   */
+  virtual CPU::Function erase(SubRequest *sreq) = 0;
+
+  /**
+   * \brief DMA done callback
+   *
+   * Called when DMA operation (Host <-> DRAM) has been completed.
+   *
+   * \param[in] lpn LPN address than completed
+   */
+  virtual void dmaDone(LPN lpn) = 0;
+
+  /**
+   * \brief NVM done callback
+   *
+   * Called when DMA operation (DRAM <-> NVM) has been completed.
+   *
+   * \param[in] lpn LPN address than completed
+   */
+  virtual void nvmDone(LPN lpn) = 0;
+
+  /**
+   * \brief Drain done callback
+   *
+   * Called when drain (write-back) operation has been completed.
+   */
   virtual void drainDone() = 0;
 };
 
