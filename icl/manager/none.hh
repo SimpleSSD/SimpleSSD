2600c43ad7e9806db076ddff2385f8c647ac7e52
Donghyun Gouk <kukdh1@gmail.com>
2020-01-29 20:59:45 +0900

    icl: Add nocache model

---
+++ icl/manager/none.hh
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#pragma once
+
+#ifndef __SIMPLESSD_ICL_MANAGER_NONE_HH__
+#define __SIMPLESSD_ICL_MANAGER_NONE_HH__
+
+#include "icl/manager/abstract_manager.hh"
+
+namespace SimpleSSD::ICL {
+
+class NoCache : public AbstractManager {
+ public:
+  NoCache(ObjectData &, FTL::FTL *, Event);
+  ~NoCache();
+
+  void read(SubRequest *) override;
+  void write(SubRequest *) override;
+  void flush(SubRequest *) override;
+  void erase(SubRequest *) override;
+  void dmaDone(SubRequest *) override;
+
+  void drain(std::vector<FlushContext> &) override;
+
+  void getStatList(std::vector<Stat> &, std::string) noexcept override;
+  void getStatValues(std::vector<double> &) noexcept override;
+  void resetStatValues() noexcept override;
+
+  void createCheckpoint(std::ostream &) const noexcept override;
+  void restoreCheckpoint(std::istream &) noexcept override;
+};
+
+}  // namespace SimpleSSD::ICL
+
+#endif
