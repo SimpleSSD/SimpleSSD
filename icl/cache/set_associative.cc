223994f58d2c897d9721240e7cecee7f0bd66f41
Donghyun Gouk <kukdh1@gmail.com>
2020-01-30 11:40:22 +0900

    icl: Writing set-associative cache

---
+++ icl/cache/set_associative.cc
@@ -0,0 +1,231 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#include "icl/cache/set_associative.hh"
+
+#include "icl/manager/abstract_manager.hh"
+#include "util/algorithm.hh"
+
+namespace SimpleSSD::ICL {
+
+SetAssociative::SetAssociative(ObjectData &o, AbstractManager *m,
+                               FTL::Parameter *p)
+    : AbstractCache(o, m, p), gen(rd()) {
+  evictMode = (Config::Granularity)readConfigUint(
+      Section::InternalCache, Config::Key::EvictGranularity);
+
+  auto policy = (Config::EvictPolicyType)readConfigUint(
+      Section::InternalCache, Config::Key::EvictPolicy);
+
+  uint32_t lineSize = parameter->pageSize;
+
+  sectorsInCacheLine = lineSize / minIO;
+
+  // Allocate cachelines
+  setSize = 0;
+  waySize = (uint32_t)readConfigUint(Section::InternalCache,
+                                     Config::Key::CacheWaySize);
+
+  cacheSize = readConfigUint(Section::InternalCache, Config::Key::CacheSize);
+
+  if (waySize == 0) {
+    // Fully-associative
+    setSize = 1;
+    waySize = MAX(cacheSize / lineSize, 1);
+  }
+  else {
+    setSize = MAX(cacheSize / lineSize / waySize, 1);
+  }
+
+  cacheline.reserve(setSize * waySize);
+
+  for (uint64_t i = 0; i < (uint64_t)setSize * waySize; i++) {
+    cacheline.emplace_back(CacheLine(sectorsInCacheLine));
+  }
+
+  cacheSize = (uint64_t)setSize * waySize * lineSize;
+
+  debugprint(
+      Log::DebugID::ICL_SetAssociative,
+      "CREATE  | Set size %u | Way size %u | Line size %u | Capacity %" PRIu64,
+      setSize, waySize, lineSize, cacheSize);
+
+  // Allocate memory
+  cacheDataBaseAddress = object.memory->allocate(
+      cacheSize, Memory::MemoryType::DRAM, "ICL::SetAssociative::Data");
+
+  // Replace insertedAt and accessedAt as 2 byte clock-LRU
+  cacheTagSize = 3 + DIVCEIL(sectorsInCacheLine, 8);
+
+  // Try SRAM
+  if (object.memory->allocate(cacheTagSize, Memory::MemoryType::SRAM, "",
+                              true) == 0) {
+    cacheTagBaseAddress = object.memory->allocate(
+        cacheTagSize, Memory::MemoryType::SRAM, "ICL::SetAssociative::Tag");
+  }
+  else {
+    cacheTagBaseAddress = object.memory->allocate(
+        cacheTagSize, Memory::MemoryType::DRAM, "ICL::SetAssociative::Tag");
+  }
+
+  // Create victim cacheline selecttion
+  switch (policy) {
+    case Config::EvictPolicyType::Random:
+      dist = std::uniform_int_distribution<uint32_t>(0, waySize - 1);
+
+      evictFunction = [this](uint32_t s, uint32_t &w) -> CPU::Function {
+        return randomEviction(s, w);
+      };
+
+      break;
+    case Config::EvictPolicyType::FIFO:
+      evictFunction = [this](uint32_t s, uint32_t &w) -> CPU::Function {
+        return fifoEviction(s, w);
+      };
+
+      break;
+    case Config::EvictPolicyType::LRU:
+      evictFunction = [this](uint32_t s, uint32_t &w) -> CPU::Function {
+        return lruEviction(s, w);
+      };
+
+      break;
+    default:
+      panic("Unexpected eviction policy.");
+
+      break;
+  }
+}
+
+SetAssociative::~SetAssociative() {}
+
+CPU::Function SetAssociative::randomEviction(uint32_t set, uint32_t &way) {
+  CPU::Function fstat;
+  CPU::markFunction(fstat);
+
+  do {
+    way = dist(gen);
+
+    auto &line = cacheline.at(set * waySize + way);
+
+    if (line.valid) {
+      break;
+    }
+  } while (true);
+
+  return fstat;
+}
+
+CPU::Function SetAssociative::fifoEviction(uint32_t set, uint32_t &way) {
+  CPU::Function fstat;
+  CPU::markFunction(fstat);
+
+  uint64_t min = std::numeric_limits<uint64_t>::max();
+
+  for (uint32_t i = 0; i < waySize; i++) {
+    if (cacheline[set * waySize + i].insertedAt < min) {
+      min = cacheline[set * waySize + i].insertedAt;
+      way = i;
+    }
+  }
+
+  return fstat;
+}
+
+CPU::Function SetAssociative::lruEviction(uint32_t set, uint32_t &way) {
+  CPU::Function fstat;
+  CPU::markFunction(fstat);
+
+  uint64_t min = std::numeric_limits<uint64_t>::max();
+
+  for (uint32_t i = 0; i < waySize; i++) {
+    if (cacheline[set * waySize + i].accessedAt < min) {
+      min = cacheline[set * waySize + i].accessedAt;
+      way = i;
+    }
+  }
+
+  return fstat;
+}
+
+CPU::Function SetAssociative::lookup(SubRequest *sreq, bool read) {
+  CPU::Function fstat;
+  CPU::markFunction(fstat);
+
+  LPN lpn = sreq->getLPN();
+
+  auto iter = cacheline.find(lpn);
+
+  if (iter != cacheline.end()) {
+    // Hit
+    sreq->setHit();
+  }
+  else if (!read) {
+    // Write, check conflict-miss
+  }
+
+  return fstat;
+}
+
+CPU::Function SetAssociative::allocate(SubRequest *sreq) {
+  CPU::Function fstat;
+  CPU::markFunction(fstat);
+
+  // Check capacity
+  if (cacheline.size() < maxEntryCount) {
+    // Just add new entry for current subrequest
+    auto ret = cacheline.emplace(sreq->getLPN(), CacheLine(sectorsInCacheLine));
+
+    panic_if(!ret.second, "Cache corrupted.");
+  }
+  else {
+    // Select cachelines to evict
+    uint32_t nl = parameter->parallelismLevel[0];
+    std::vector<FlushContext> list;
+
+    if (evictMode == Config::Granularity::AllLevel) {
+      nl = parameter->parallelism;
+    }
+
+    for (uint32_t i = 0; i < nl; i++) {
+      auto iter = evictFunction();
+
+      panic_if(iter == cacheline.end(), "Cache corrupted.");
+
+      list.emplace_back(FlushContext(iter->first, iter->second.address));
+    }
+
+    manager->drain(list);
+  }
+
+  return fstat;
+}
+
+CPU::Function SetAssociative::flush(SubRequest *sreq) {
+  CPU::Function fstat;
+  CPU::markFunction(fstat);
+
+  return fstat;
+}
+
+CPU::Function SetAssociative::erase(SubRequest *) {}
+
+void SetAssociative::dmaDone(LPN) {}
+
+void SetAssociative::drainDone() {}
+
+void SetAssociative::getStatList(std::vector<Stat> &, std::string) noexcept {}
+
+void SetAssociative::getStatValues(std::vector<double> &) noexcept {}
+
+void SetAssociative::resetStatValues() noexcept {}
+
+void SetAssociative::createCheckpoint(std::ostream &) const noexcept {}
+
+void SetAssociative::restoreCheckpoint(std::istream &) noexcept {}
+
+}  // namespace SimpleSSD::ICL
---
+++ icl/cache/set_associative.hh
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#pragma once
+
+#ifndef __SIMPLESSD_ICL_CACHE_SET_ASSOCIATIVE_HH__
+#define __SIMPLESSD_ICL_CACHE_SET_ASSOCIATIVE_HH__
+
+#include <random>
+
+#include "icl/cache/abstract_cache.hh"
+#include "util/bitset.hh"
+
+namespace SimpleSSD::ICL {
+
+class SetAssociative : public AbstractCache {
+ protected:
+  struct CacheLine {
+    union {
+      uint8_t data;
+      struct {
+        uint8_t valid : 1;
+        uint8_t dirty : 1;
+        uint8_t nvmPending : 1;
+        uint8_t dmaPending : 1;
+        uint8_t rsvd : 4;
+      };
+    };
+
+    uint64_t insertedAt;
+    uint64_t accessedAt;
+    Bitset validbits;
+
+    CacheLine(uint64_t size)
+        : insertedAt(0), accessedAt(0), data(0), validbits(size) {}
+  };
+
+  uint32_t sectorsInCacheLine;
+  uint32_t setSize;
+  uint32_t waySize;
+
+  Config::Granularity evictMode;
+
+  uint64_t cacheTagBaseAddress;
+  uint64_t cacheDataBaseAddress;
+  uint64_t cacheTagSize;
+  uint64_t cacheSize;
+
+  std::vector<CacheLine> cacheline;
+  std::function<CPU::Function(uint32_t, uint32_t &)> evictFunction;
+
+  // Victim selection
+  std::random_device rd;
+  std::mt19937 gen;
+  std::uniform_int_distribution<uint32_t> dist;
+
+  CPU::Function randomEviction(uint32_t, uint32_t &);
+  CPU::Function fifoEviction(uint32_t, uint32_t &);
+  CPU::Function lruEviction(uint32_t, uint32_t &);
+
+ public:
+  SetAssociative(ObjectData &, AbstractManager *, FTL::Parameter *);
+  ~SetAssociative();
+
+  CPU::Function lookup(SubRequest *, bool) override;
+  CPU::Function allocate(SubRequest *) override;
+  CPU::Function flush(SubRequest *) override;
+  CPU::Function erase(SubRequest *) override;
+  void dmaDone(LPN) override;
+  void drainDone() override;
+
+  void getStatList(std::vector<Stat> &, std::string) noexcept override;
+  void getStatValues(std::vector<double> &) noexcept override;
+  void resetStatValues() noexcept override;
+
+  void createCheckpoint(std::ostream &) const noexcept override;
+  void restoreCheckpoint(std::istream &) noexcept override;
+};
+
+}  // namespace SimpleSSD::ICL
+
+#endif
