75e31e2155ef315360192c5691cfe0c40adb9513
Donghyun Gouk <kukdh1@gmail.com>
2020-09-13 05:45:08 +0900

    hil/none: Add missing function

--- hil/none/subsystem.hh
+++ hil/none/subsystem.hh
@@ -33,6 +33,8 @@ class Subsystem : public AbstractSubsystem {
   ControllerID createController(Interface *) noexcept override;
   AbstractController *getController(ControllerID) noexcept override;
 
+  void getGCHint(FTL::GC::HintContext &ctx) noexcept override;
+
   void getStatList(std::vector<Stat> &, std::string) noexcept override;
   void getStatValues(std::vector<double> &) noexcept override;
   void resetStatValues() noexcept override;
