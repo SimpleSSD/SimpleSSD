8151a105ad5ed573691f546438662166190f6867
Donghyun Gouk <kukdh1@gmail.com>
2020-09-22 01:40:44 +0900

    ftl/gc: Now AdvancedGC become simple again

--- ftl/gc/advanced.cc
+++ ftl/gc/advanced.cc
@@ -13,7 +13,7 @@
 namespace SimpleSSD::FTL::GC {
 
 AdvancedGC::AdvancedGC(ObjectData &o, FTLObjectData &fo, FIL::FIL *f)
-    : NaiveGC(o, fo, f), pendingFIL(0) {
+    : NaiveGC(o, fo, f) {
   logid = Log::DebugID::FTL_AdvancedGC;
 
   idletime = readConfigUint(Section::FlashTranslation,
@@ -32,41 +32,16 @@ AdvancedGC::~AdvancedGC() {}
 
 void AdvancedGC::triggerBackground(uint64_t now) {
   if (UNLIKELY(ftlobject.pAllocator->checkBackgroundGCThreshold() &&
-               state < State::Foreground)) {
-    if (state == State::Paused) {
-      resumePaused();
-    }
-    else {
-      scheduleNow(eventTrigger);
-    }
-
+               state == State::Idle)) {
     state = State::Background;
     beginAt = now;
-  }
-}
 
-void AdvancedGC::triggerForeground() {
-  if (UNLIKELY(ftlobject.pAllocator->checkForegroundGCThreshold() &&
-               state < State::Foreground)) {
-    if (state == State::Paused) {
-      resumePaused();
-    }
-    else {
-      scheduleNow(eventTrigger);
-    }
-
-    state = State::Foreground;
-    beginAt = getTick();
+    scheduleNow(eventTrigger);
   }
 }
 
 void AdvancedGC::requestArrived(Request *req) {
-  // Request preemption
-  if (UNLIKELY(state >= State::Foreground && !preemptRequested())) {
-    debugprint(logid, "GC    | Preemption requested");
-  }
-
-  // Penalty calculation & Background GC invocation
+  // Penalty calculation
   NaiveGC::requestArrived(req);
 
   // Not scheduled
@@ -80,48 +55,25 @@ void AdvancedGC::requestArrived(Request *req) {
   }
 }
 
-void AdvancedGC::resumePaused() {
-  debugprint(logid, "GC    | Resume from preempted state");
-
-  for (auto &iter : ongoingCopy) {
-    auto &block = iter.second;
-
-    panic_if(block.writeCounter != 0 || block.readCounter != 0,
-             "Unexpected GC preemption state");
-
-    if (block.pageWriteIndex == block.pageReadIndex) {
-      scheduleNow(eventDoRead, iter.first);
-    }
-    else {
-      panic("Unexpected GC preemption state");
-    }
-  }
-}
-
 void AdvancedGC::gc_trigger() {
   // Get blocks to erase
-  ftlobject.pAllocator->getVictimBlocks(blockList, eventStart);
+  ftlobject.pAllocator->getVictimBlocks(targetBlock, eventStart);
 
   if (ftlobject.pAllocator->checkForegroundGCThreshold()) {
     stat.fgcCount++;
     state = State::Foreground;
 
-    debugprint(logid, "GC    | Foreground | %u (super)blocks",
-               blockList.size());
+    debugprint(logid, "GC    | Foreground");
   }
   else if (state == State::Background) {
     stat.bgcCount++;
     state = State::Background;
 
-    debugprint(logid, "GC    | Background | %u (super)blocks",
-               blockList.size());
+    debugprint(logid, "GC    | Background");
   }
 }
 
 void AdvancedGC::gc_checkDone(uint64_t now) {
-  // Maybe GC is completed while waiting for pending requests
-  checkPreemptible();
-
   // Triggered GC completed
   if (state == State::Foreground) {
     debugprint(logid,
@@ -140,112 +92,15 @@ void AdvancedGC::gc_checkDone(uint64_t now) {
   triggerForeground();
 }
 
-void AdvancedGC::gc_doRead(uint64_t now, uint64_t tag) {
-  LPN invlpn;
-  auto &block = findCopySession(tag);
-
-  if (LIKELY(block.pageReadIndex < block.copyList.size())) {
-    // Do read
-    auto &ctx = block.copyList.at(block.pageReadIndex++);
-    auto ppn = param->makePPN(block.blockID, 0, ctx.pageIndex);
-
-    if (superpage > 1) {
-      debugprint(logid, "GC    | READ  | PSBN %" PRIx64 "h | PSPN %" PRIx64 "h",
-                 block.blockID, param->getPSPNFromPPN(ppn));
-    }
-    else {
-      debugprint(logid, "GC    | READ  | PBN %" PRIx64 "h | PPN %" PRIx64 "h",
-                 block.blockID, ppn);
-    }
-
-    for (uint32_t i = 0; i < superpage; i++) {
-      ppn = param->makePPN(block.blockID, i, ctx.pageIndex);
-
-      // Fill request
-      if (i == 0) {
-        ctx.request.setPPN(ppn);
-        ctx.request.setDRAMAddress(makeBufferAddress(i, ctx.pageIndex));
-        ctx.request.setTag(tag);
-
-        pFIL->read(FIL::Request(&ctx.request, eventDoTranslate));
-      }
-      else {
-        pFIL->read(FIL::Request(invlpn, ppn,
-                                makeBufferAddress(i, ctx.pageIndex),
-                                eventDoTranslate, tag));
-      }
-    }
-
-    block.readCounter = superpage;
-    ctx.beginAt = now;
-    stat.gcCopiedPages += superpage;
-
-    increasePendingFIL();
-  }
-  else if (LIKELY(!preemptRequested())) {
-    // Do erase
-    PSBN psbn = block.blockID;
-
-    // Erase
-    if (superpage > 1) {
-      debugprint(logid, "GC    | ERASE | PSBN %" PRIx64 "h", psbn);
-    }
-    else {
-      // PSBN == PBN when superpage == 1
-      debugprint(logid, "GC    | ERASE | PBN %" PRIx64 "h", psbn);
-    }
-
-    for (uint32_t i = 0; i < superpage; i++) {
-      pFIL->erase(
-          FIL::Request(invlpn, param->makePPN(psbn, i, 0), 0, eventDone, tag));
-    }
-
-    block.beginAt = now;
-    block.writeCounter = superpage;  // Reuse
-    stat.gcErasedBlocks += superpage;
-
-    increasePendingFIL();
-  }
-  else {
-    checkPreemptible();
-  }
-}
-
-void AdvancedGC::gc_doTranslate(uint64_t now, uint64_t tag) {
-  decreasePendingFIL();
-
-  NaiveGC::gc_doTranslate(now, tag);
-}
-
-void AdvancedGC::gc_doWrite(uint64_t now, uint64_t tag) {
-  NaiveGC::gc_doWrite(now, tag);
-
-  increasePendingFIL();
-}
-
-void AdvancedGC::gc_writeDone(uint64_t now, uint64_t tag) {
-  decreasePendingFIL();
-
-  NaiveGC::gc_writeDone(now, tag);
-}
-
-void AdvancedGC::gc_done(uint64_t now, uint64_t tag) {
-  decreasePendingFIL();
-
-  NaiveGC::gc_done(now, tag);
-}
-
 void AdvancedGC::createCheckpoint(std::ostream &out) const noexcept {
   NaiveGC::createCheckpoint(out);
 
-  BACKUP_SCALAR(out, pendingFIL);
   BACKUP_EVENT(out, eventBackgroundGC);
 }
 
 void AdvancedGC::restoreCheckpoint(std::istream &in) noexcept {
   NaiveGC::restoreCheckpoint(in);
 
-  RESTORE_SCALAR(in, pendingFIL);
   RESTORE_EVENT(in, eventBackgroundGC);
 }
 
--- ftl/gc/advanced.hh
+++ ftl/gc/advanced.hh
@@ -17,46 +17,17 @@ namespace SimpleSSD::FTL::GC {
 class AdvancedGC : public NaiveGC {
  protected:
   uint64_t idletime;
-  uint64_t pendingFIL;  // Pending FIL requests (read/program/erase)
-
-  inline bool preemptRequested() {
-    return firstRequestArrival < std::numeric_limits<uint64_t>::max();
-  }
-
-  inline void increasePendingFIL() { pendingFIL += superpage; }
-  inline void decreasePendingFIL() { pendingFIL--; }
-
-  virtual inline void checkPreemptible() {
-    if (UNLIKELY(pendingFIL == 0)) {
-      state = State::Paused;
-
-      debugprint(logid, "GC    | Preempted");
-
-      // Calculate penalty here
-      updatePenalty(getTick());
-
-      firstRequestArrival = std::numeric_limits<uint64_t>::max();
-    }
-  }
-
-  void resumePaused();
 
   Event eventBackgroundGC;
   virtual void triggerBackground(uint64_t);
 
   void gc_trigger() override;
   void gc_checkDone(uint64_t) override;
-  void gc_doRead(uint64_t, uint64_t) override;
-  void gc_doTranslate(uint64_t, uint64_t) override;
-  void gc_doWrite(uint64_t, uint64_t) override;
-  void gc_writeDone(uint64_t, uint64_t) override;
-  void gc_done(uint64_t, uint64_t) override;
 
  public:
   AdvancedGC(ObjectData &, FTLObjectData &, FIL::FIL *);
   virtual ~AdvancedGC();
 
-  void triggerForeground() override;
   void requestArrived(Request *) override;
 
   void createCheckpoint(std::ostream &) const noexcept override;
