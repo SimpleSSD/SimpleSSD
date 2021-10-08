7fc749563f180ced2aaebb5d86945aab1c6356cc
Donghyun Gouk <kukdh1@gmail.com>
2021-10-08 17:23:40 +0900

    ftl/wear_leveling: Add abstract wear leveling

---
+++ ftl/wear_leveling/abstract_wear_leveling.cc
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#include "ftl/wear_leveling/abstract_wear_leveling.hh"
+
+namespace SimpleSSD::FTL::WearLeveling {
+
+AbstractWearLeveling::AbstractWearLeveling(ObjectData &o, FTLObjectData &fo,
+                                           FIL::FIL *fil)
+    : AbstractBlockCopyJob(o, fo, fil), state(State::Idle) {
+  eventEraseCallback = createEvent(
+      [this](uint64_t t, uint64_t d) { blockEraseCallback(t, PSBN{d}); },
+      "FTL::WearLeveling::eventEraseCallback");
+}
+
+AbstractWearLeveling::~AbstractWearLeveling() {}
+
+void AbstractWearLeveling::blockEraseCallback(uint64_t, const PSBN &) {
+  panic("AbstractWearLeveling::blockEraseCallback() must be overriden.");
+}
+
+void AbstractWearLeveling::initialize() {}
+
+bool AbstractWearLeveling::isRunning() {
+  return state >= State::Foreground;
+}
+
+void AbstractWearLeveling::createCheckpoint(std::ostream &out) const noexcept {
+  BACKUP_SCALAR(out, state);
+  BACKUP_EVENT(out, eventEraseCallback);
+}
+
+void AbstractWearLeveling::restoreCheckpoint(std::istream &in) noexcept {
+  RESTORE_SCALAR(in, state);
+  RESTORE_EVENT(in, eventEraseCallback);
+}
+
+}  // namespace SimpleSSD::FTL::WearLeveling
---
+++ ftl/wear_leveling/abstract_wear_leveling.hh
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#pragma once
+
+#ifndef __SIMPLESSD_FTL_WEAR_LEVELING_ABSTRACT_WEAR_LEVELING_HH__
+#define __SIMPLESSD_FTL_WEAR_LEVELING_ABSTRACT_WEAR_LEVELING_HH__
+
+#include <random>
+
+#include "fil/fil.hh"
+#include "ftl/allocator/victim_selection.hh"
+#include "ftl/background_manager/abstract_background_job.hh"
+#include "ftl/def.hh"
+
+namespace SimpleSSD::FTL::WearLeveling {
+
+enum class State : uint32_t {
+  Idle,
+
+  Foreground,
+  Background,
+};
+
+class AbstractWearLeveling : public AbstractBlockCopyJob {
+ protected:
+  State state;
+
+  uint32_t getParallelBlockCount() { return 1; }
+
+  Event eventEraseCallback;
+  virtual void blockEraseCallback(uint64_t now, const PSBN &erased);
+
+  std::string getPrefix() override { return "FTL::WearLeveling"; }
+  const char *getLogPrefix() override { return "WL    "; }
+
+ public:
+  AbstractWearLeveling(ObjectData &, FTLObjectData &, FIL::FIL *);
+  virtual ~AbstractWearLeveling();
+
+  void initialize() override;
+  bool isRunning() override;
+
+  void createCheckpoint(std::ostream &) const noexcept override;
+  void restoreCheckpoint(std::istream &) noexcept override;
+};
+
+}  // namespace SimpleSSD::FTL::WearLeveling
+
+#endif
