1e53a3d05bfb5eb97d6cfdb7f9e2d90168e69739
Donghyun Gouk <kukdh1@gmail.com>
2021-10-08 13:55:20 +0900

    ftl/background_manager: Add abstract block copy job

---
+++ ftl/background_manager/abstract_background_job.cc
@@ -0,0 +1,262 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+/*
+ * Copyright (C) 2019 CAMELab
+ *
+ * Author: Donghyun Gouk <kukdh1@camelab.org>
+ */
+
+#include "ftl/background_manager/abstract_background_job.hh"
+
+#include "ftl/allocator/abstract_allocator.hh"
+#include "ftl/mapping/abstract_mapping.hh"
+
+namespace SimpleSSD::FTL {
+
+AbstractJob::AbstractJob(ObjectData &o, FTLObjectData &fo)
+    : Object(o), ftlobject(fo) {}
+
+AbstractJob::~AbstractJob() {}
+
+void AbstractJob::triggerByUser(TriggerType, Request *) {}
+
+void AbstractJob::triggerByIdle(uint64_t, uint64_t) {}
+
+AbstractBlockCopyJob::AbstractBlockCopyJob(ObjectData &o, FTLObjectData &fo,
+                                           FIL::FIL *fil)
+    : AbstractJob(o, fo),
+      pFIL(fil),
+      param(ftlobject.pMapping->getInfo()),
+      bufferBaseAddress(0),
+      superpage(param->pageSize),
+      pageSize(object.config->getNANDStructure()->pageSize),
+      logid(getDebugLogID()),
+      logprefix(getLogPrefix()) {
+  auto prefix = getPrefix();
+
+  // Memory allocation
+  auto &_bufferBaseAddress = const_cast<uint64_t &>(bufferBaseAddress);
+  auto required = superpage * pageSize;
+
+  if (object.memory->allocate(required, Memory::MemoryType::SRAM, "", true) ==
+      0) {
+    _bufferBaseAddress = object.memory->allocate(
+        required, Memory::MemoryType::SRAM, prefix + "Buffer");
+  }
+  else {
+    _bufferBaseAddress = object.memory->allocate(
+        required, Memory::MemoryType::DRAM, prefix + "Buffer");
+  }
+
+  // Create events
+  eventReadPage = createEvent([this](uint64_t t, uint64_t) { readPage(t); },
+                              prefix + "eventReadPage");
+  eventUpdateMapping =
+      createEvent([this](uint64_t t, uint64_t) { updateMapping(t); },
+                  prefix + "eventUpdateMapping");
+  eventWritePage = createEvent([this](uint64_t t, uint64_t) { writePage(t); },
+                               prefix + "eventWritePage");
+  eventWriteDone = createEvent([this](uint64_t t, uint64_t) { writeDone(t); },
+                               prefix + "eventWriteDone");
+  eventEraseDone = createEvent([this](uint64_t t, uint64_t) { eraseDone(t); },
+                               prefix + "eventEraseDone");
+  eventDone = createEvent([this](uint64_t t, uint64_t) { done(t); },
+                          prefix + "eventDone");
+}
+
+AbstractBlockCopyJob::~AbstractBlockCopyJob() {}
+
+void AbstractBlockCopyJob::readPage(uint64_t now) {
+  if (LIKELY(targetBlock.pageReadIndex < targetBlock.copyList.size())) {
+    auto &ctx = targetBlock.copyList.at(targetBlock.pageReadIndex++);
+    auto ppn = param->makePPN(targetBlock.blockID, 0, ctx.pageIndex);
+
+    if (superpage > 1) {
+      debugprint(logid, "%s| READ  | PSBN %" PRIx64 "h | PSPN %" PRIx64 "h",
+                 logprefix, targetBlock.blockID, param->getPSPNFromPPN(ppn));
+    }
+    else {
+      debugprint(logid, "%s| READ  | PBN %" PRIx64 "h | PPN %" PRIx64 "h",
+                 logprefix, targetBlock.blockID, ppn);
+    }
+
+    for (uint32_t i = 0; i < superpage; i++) {
+      ppn = param->makePPN(targetBlock.blockID, i, ctx.pageIndex);
+
+      // Fill request
+      if (i == 0) {
+        ctx.request.setPPN(ppn);
+        ctx.request.setDRAMAddress(makeBufferAddress(i));
+
+        pFIL->read(FIL::Request(&ctx.request, eventUpdateMapping));
+      }
+      else {
+        pFIL->read(FIL::Request(LPN{}, ppn, makeBufferAddress(i),
+                                eventUpdateMapping, 0));
+      }
+    }
+
+    targetBlock.readCounter = superpage;
+    ctx.beginAt = now;
+  }
+  else {
+    // Do erase
+    PSBN psbn = targetBlock.blockID;
+
+    // Erase
+    if (superpage > 1) {
+      debugprint(logid, "%s| ERASE | PSBN %" PRIx64 "h", logprefix, psbn);
+    }
+    else {
+      // PSBN == PBN when superpage == 1
+      debugprint(logid, "%s| ERASE | PBN %" PRIx64 "h", logprefix, psbn);
+    }
+
+    for (uint32_t i = 0; i < superpage; i++) {
+      pFIL->erase(FIL::Request(LPN{}, param->makePPN(psbn, i, 0), 0,
+                               eventEraseDone, 0));
+    }
+
+    targetBlock.beginAt = now;
+    targetBlock.writeCounter = superpage;  // Reuse
+  }
+}
+
+void AbstractBlockCopyJob::updateMapping(uint64_t now) {
+  targetBlock.readCounter--;
+
+  if (targetBlock.readCounter == 0) {
+    // Start address translation
+    auto &ctx = targetBlock.copyList.at(targetBlock.pageWriteIndex);
+    auto lpn = ctx.request.getLPN();
+    auto ppn = ctx.request.getPPN();
+
+    panic_if(!lpn.isValid(), "Invalid LPN received.");
+
+    if (superpage > 1) {
+      debugprint(
+          logid,
+          "%s| READ  | PSBN %" PRIx64 "h | PSPN %" PRIx64 "h -> LSPN %" PRIx64
+          "h | %" PRIu64 " - %" PRIu64 " (%" PRIu64 ")",
+          logprefix, targetBlock.blockID, param->getPSPNFromPPN(ppn),
+          param->getLSPNFromLPN(lpn), ctx.beginAt, now, now - ctx.beginAt);
+    }
+    else {
+      debugprint(logid,
+                 "%s| READ  | PBN %" PRIx64 "h | PPN %" PRIx64
+                 "h -> LPN %" PRIx64 "h | %" PRIu64 " - %" PRIu64 " (%" PRIu64
+                 ")",
+                 logprefix, targetBlock.blockID, ppn, lpn, ctx.beginAt, now,
+                 now - ctx.beginAt);
+    }
+
+    ftlobject.pMapping->writeMapping(&ctx.request, eventWritePage, true);
+  }
+}
+
+void AbstractBlockCopyJob::writePage(uint64_t now) {
+  auto &ctx = targetBlock.copyList.at(targetBlock.pageWriteIndex++);
+  auto lpn = ctx.request.getLPN();
+  auto ppn = ctx.request.getPPN();
+
+  if (superpage > 1) {
+    debugprint(logid,
+               "%s| WRITE | PSBN %" PRIx64 "h | LSPN %" PRIx64
+               "h -> PSPN %" PRIx64 "h",
+               logprefix, targetBlock.blockID, param->getLSPNFromLPN(lpn),
+               param->getPSPNFromPPN(ppn));
+  }
+  else {
+    debugprint(logid,
+               "%s| WRITE | PBN %" PRIx64 "h | LPN %" PRIx64 "h -> PPN %" PRIx64
+               "h",
+               logprefix, targetBlock.blockID, lpn, ppn);
+  }
+
+  for (uint32_t i = 0; i < superpage; i++) {
+    pFIL->program(FIL::Request(static_cast<LPN>(lpn + i),
+                               static_cast<PPN>(ppn + i), makeBufferAddress(i),
+                               eventWriteDone, 0));
+  }
+
+  targetBlock.writeCounter += superpage;  // Do not overwrite
+  ctx.beginAt = now;
+}
+
+void AbstractBlockCopyJob::writeDone(uint64_t now) {
+  targetBlock.writeCounter--;
+
+  if (targetBlock.writeCounter == 0) {
+    auto &ctx = targetBlock.copyList.at(targetBlock.pageWriteIndex - 1);
+    auto lpn = ctx.request.getLPN();
+    auto ppn = ctx.request.getPPN();
+
+    if (superpage > 1) {
+      debugprint(
+          logid,
+          "%s| WRITE | PSBN %" PRIx64 "h | LSPN %" PRIx64 "h -> PSPN %" PRIx64
+          "h | %" PRIu64 " - %" PRIu64 " (%" PRIu64 ")",
+          logprefix, targetBlock.blockID, param->getLSPNFromLPN(lpn),
+          param->getPSPNFromPPN(ppn), ctx.beginAt, now, now - ctx.beginAt);
+    }
+    else {
+      debugprint(logid,
+                 "%s| WRITE | PBN %" PRIx64 "h | LPN %" PRIx64
+                 "h -> PPN %" PRIx64 "h | %" PRIu64 " - %" PRIu64 " (%" PRIu64
+                 ")",
+                 logprefix, targetBlock.blockID, lpn, ppn, ctx.beginAt, now,
+                 now - ctx.beginAt);
+    }
+
+    // Go back to read
+    scheduleNow(eventReadPage);
+  }
+}
+
+void AbstractBlockCopyJob::eraseDone(uint64_t now) {
+  targetBlock.writeCounter--;
+
+  if (targetBlock.writeCounter == 0) {
+    // Erase completed
+    if (superpage > 1) {
+      debugprint(logid,
+                 "%s| ERASE | PSBN %" PRIx64 "h | %" PRIu64 " - %" PRIu64
+                 " (%" PRIu64 ")",
+                 logprefix, targetBlock.blockID, targetBlock.beginAt, now,
+                 now - targetBlock.beginAt);
+    }
+    else {
+      debugprint(logid,
+                 "%s| ERASE | PBN %" PRIx64 "h | %" PRIu64 " - %" PRIu64
+                 " (%" PRIu64 ")",
+                 logprefix, targetBlock.blockID, targetBlock.beginAt, now,
+                 now - targetBlock.beginAt);
+    }
+
+    // Mark table/block as erased
+    ftlobject.pAllocator->reclaimBlock(targetBlock.blockID, eventDone, 0);
+  }
+}
+
+void AbstractBlockCopyJob::createCheckpoint(std::ostream &out) const noexcept {
+  targetBlock.createCheckpoint(out);
+
+  BACKUP_EVENT(out, eventReadPage);
+  BACKUP_EVENT(out, eventUpdateMapping);
+  BACKUP_EVENT(out, eventWritePage);
+  BACKUP_EVENT(out, eventWriteDone);
+  BACKUP_EVENT(out, eventEraseDone);
+  BACKUP_EVENT(out, eventDone);
+}
+
+void AbstractBlockCopyJob::restoreCheckpoint(std::istream &in) noexcept {
+  targetBlock.restoreCheckpoint(in);
+
+  RESTORE_EVENT(in, eventReadPage);
+  RESTORE_EVENT(in, eventUpdateMapping);
+  RESTORE_EVENT(in, eventWritePage);
+  RESTORE_EVENT(in, eventWriteDone);
+  RESTORE_EVENT(in, eventEraseDone);
+  RESTORE_EVENT(in, eventDone);
+}
+
+}  // namespace SimpleSSD::FTL
--- ftl/background_manager/abstract_background_job.hh
+++ ftl/background_manager/abstract_background_job.hh
@@ -10,6 +10,7 @@
 #ifndef __SIMPLESSD_FTL_BACKGROUND_MANAGER_ABSTRACT_BACKGROUND_JOB_HH__
 #define __SIMPLESSD_FTL_BACKGROUND_MANAGER_ABSTRACT_BACKGROUND_JOB_HH__
 
+#include "fil/fil.hh"
 #include "ftl/def.hh"
 #include "ftl/object.hh"
 #include "sim/object.hh"
@@ -65,6 +66,108 @@ class AbstractJob : public Object {
    * \param deadline Expected timestamp of next user I/O
    */
   virtual void triggerByIdle(uint64_t now, uint64_t deadline);
+};
+
+class AbstractBlockCopyJob : public AbstractJob {
+ protected:
+  FIL::FIL *const pFIL;
+
+  CopyContext targetBlock;
+
+  const Parameter *const param;
+  const uint64_t bufferBaseAddress;
+  const uint32_t superpage;
+  const uint32_t pageSize;
+
+  const Log::DebugID logid;
+  const char *const logprefix;
+
+  //!< Get prefix of objects
+  virtual std::string getPrefix() = 0;
+
+  //!< Get prefix of log
+  virtual const char *getLogPrefix() = 0;
+
+  //!< Get debug lod ID
+  virtual Log::DebugID getDebugLogID() = 0;
+
+  //!< Helper function to calculate offset of buffer
+  inline uint64_t makeBufferAddress(uint32_t superpageIndex) {
+    return bufferBaseAddress + superpageIndex * pageSize;
+  }
+
+  /*
+   * State machine for block copy.
+   *
+   * Read Page -> Update mapping table -> Write Page -.
+   *   |    `-------------------------------------<---'
+   *   `--------> Erase block ---> Done
+   */
+
+  /**
+   * \brief Perform page read or erase
+   *
+   * This event is intended to be completion handler of
+   * AbstractAllocator::getVictimBlock.
+   *
+   * If we copied all valid pages in CopyContext:
+   *   Issue Erase request to FIL with completion handler of eventEraseDone.
+   * Else:
+   *   Issue Read request to FIL with completion handler of eventUpdateMapping.
+   */
+  Event eventReadPage;
+  virtual void readPage(uint64_t);
+
+  /**
+   * \brief Perform mapping table update
+   *
+   * Ask mapping class to get where to write new page.
+   * Issue writeMapping with completion handler of eventWritePage.
+   */
+  Event eventUpdateMapping;
+  virtual void updateMapping(uint64_t);
+
+  /**
+   * \brief Perform write
+   *
+   * Issue Program request to FIL with completion handler of eventWriteDone.
+   */
+  Event eventWritePage;
+  virtual void writePage(uint64_t);
+
+  /**
+   * \brief Completion handler of write
+   *
+   * This state handles multiple program requests caused by superpage
+   * configuration. If all program requests are completed, go to eventReadPage.
+   */
+  Event eventWriteDone;
+  virtual void writeDone(uint64_t);
+
+  /**
+   * \brief Completion handler of erase
+   *
+   * This state handles multiple erase requests caused by superpage
+   * configuration. If all erase requests are completed, reclaim block by
+   * calling AbstractAllocator::reclaimBlock.
+   */
+  Event eventEraseDone;
+  virtual void eraseDone(uint64_t);
+
+  /**
+   * \brief Completion handler of copy operation
+   *
+   * This state checks termination condition.
+   */
+  Event eventDone;
+  virtual void done(uint64_t) = 0;
+
+ public:
+  AbstractBlockCopyJob(ObjectData &, FTLObjectData &, FIL::FIL *);
+  virtual ~AbstractBlockCopyJob();
+
+  void createCheckpoint(std::ostream &) const noexcept override;
+  void restoreCheckpoint(std::istream &) noexcept override;
 };
 
 }  // namespace SimpleSSD::FTL
